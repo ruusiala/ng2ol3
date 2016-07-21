a23c54064ff239f32863044c4eafd0a93bea282e
fegyi001 <padanyigulyasgergely@lltk.local>
2016-07-21 11:28:49 +0200

    asdf

--- core.d.ts
+++ core.d.ts
@@ -1,11 +1,1 @@
-export * from './src/classes/ng2ol3config';
-export * from './src/classes/ng2ol3header';
-export * from './src/classes/ng2ol3logo';
-export * from './src/classes/ng2ol3map';
-export * from './src/classes/ng2ol3sidebar';
-export * from './src/classes/ng2ol3view';
 
-export * from './src/components/ng2ol3-container.component';
-export * from './src/components/ng2ol3-header.component';
-export * from './src/components/ng2ol3-map.component';
-export * from './src/components/ng2ol3-sidebar.component';
