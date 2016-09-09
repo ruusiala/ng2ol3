f4af672ddd2fee56a755d5bf0d164903a7b1ec0c
fegyi001 <padanyigulyasgergely@lltk.local>
2016-09-09 14:10:32 +0200

    add ribbon to the demo page

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,20 +3,31 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'demo-app',
     template: `
-        <div class="demo">
-            <div class="demo-header">
-                <h1 class="title">ng2ol3 examples</h1>
-                <nav>
-                    <button routerLink="/demo-simple" routerLinkActive="active"
-                        [routerLinkActiveOptions]="{ exact: true }">simple demo</button>
-                    <button routerLink="/demo-sidebar" routerLinkActive="active"
-                        [routerLinkActiveOptions]="{ exact: true }">sidebar demo</button>
-                </nav>
-            </div>
-            <div class="demo-content">
-                <router-outlet></router-outlet>
-            </div>
+    <div>
+    <div class="ribbon-box">
+        <div class="ribbon-wrapper">
+            <a href="https://github.com/fegyi001/ng2ol3" target="_blank" title="GitHub repository">
+                <div class="ribbon">
+                    <i class="fa fa-github"></i>
+                </div>
+            </a>
         </div>
+    </div>
+    <div class="demo">
+        <div class="demo-header">
+            <h1 class="title">ng2ol3 examples</h1>
+            <nav>
+                <button routerLink="/demo-simple" routerLinkActive="active"
+                    [routerLinkActiveOptions]="{ exact: true }">simple demo</button>
+                <button routerLink="/demo-sidebar" routerLinkActive="active"
+                    [routerLinkActiveOptions]="{ exact: true }">sidebar demo</button>
+            </nav>
+        </div>
+        <div class="demo-content">
+            <router-outlet></router-outlet>
+        </div>
+    </div>
+        
     `
 })
 export class AppComponent {
